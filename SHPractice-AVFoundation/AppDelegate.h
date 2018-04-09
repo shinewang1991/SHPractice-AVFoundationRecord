b0986ea4813ca0d580501a64df4ac4639f877782
wangxiang <wangxiang@yixia.com>
2018-04-09 21:16:19 +0800

    Initial Commit

---
+++ SHPractice-AVFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHPractice-AVFoundation
+//
+//  Created by Shine on 09/04/2018.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHPractice-AVFoundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHPractice-AVFoundation
+//
+//  Created by Shine on 09/04/2018.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
