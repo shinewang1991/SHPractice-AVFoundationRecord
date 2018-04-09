588110b8fff2af037849ba056496228c146b7339
wangxiang <wangxiang@yixia.com>
2018-04-09 22:30:23 +0800

    first commit

--- SHPractice-AVFoundation/ViewController.m
+++ SHPractice-AVFoundation/ViewController.m
@@ -7,23 +7,77 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate,AVCaptureAudioDataOutputSampleBufferDelegate>
+@property (nonatomic, strong) AVCaptureDevice *cameraDevice;
+@property (nonatomic, strong) AVCaptureDevice *audioDevice;
+@property (nonatomic, strong) dispatch_queue_t sampleBufferQueue;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //需要用到的类
+    // AVCaptureSession
+    // AVCaptureInput
+    // AVCaptureOutput
+    // AVCaptureConnection
+    // AVCaptureAudioDataOutput
+    // AVCaptureVideoDataOutput
+    // AVCaptureVideoPreviewLayer 预览视图
+    // AVAssetWriter //写入文件
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    
+    //input
+    AVCaptureDeviceInput *audioInput = [AVCaptureDeviceInput deviceInputWithDevice:self.audioDevice error:NULL];
+    AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:self.cameraDevice error:NULL];
+    [session addInput:audioInput];
+    [session addInput:videoInput];
+    
+    //output
+    AVCaptureVideoDataOutput *videoOutput = [[AVCaptureVideoDataOutput alloc] init];
+    NSNumber *firstAvailableFormatType = videoOutput.availableVideoCVPixelFormatTypes.firstObject;
+//    videoOutput.videoSettings = @{kCVPixelBufferPixelFormatTypeKey:firstAvailableFormatType};
+    videoOutput.videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:firstAvailableFormatType,kCVPixelBufferPixelFormatTypeKey, nil];
+    [videoOutput setSampleBufferDelegate:self queue:self.sampleBufferQueue];
+    
+    AVCaptureAudioDataOutput *audioOutput = [[AVCaptureAudioDataOutput alloc] init];
+    [audioOutput setSampleBufferDelegate:self queue:self.sampleBufferQueue];
+
+    
+}
+
+#pragma mark - AVCaptureVideoDataOutputSampleBufferDelegate
+- (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (AVCaptureDevice *)cameraDevice{
+    if(!_cameraDevice){
+    _cameraDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionBack];   //后置摄像头
+    }
+    return _cameraDevice;
+}
 
+- (AVCaptureDevice *)audioDevice{
+    if(!_audioDevice){
+        _audioDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio]; //麦克风
+    }
+    return _audioDevice;
+}
+
+- (dispatch_queue_t)sampleBufferQueue{
+    if(!_sampleBufferQueue){
+        _sampleBufferQueue = dispatch_queue_create("shine.avfoundation", DISPATCH_QUEUE_SERIAL); //串行队列
+    }
+    return _sampleBufferQueue;
+}
 @end
